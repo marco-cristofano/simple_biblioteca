0de0da15ae2acaffb0ba22f28165c2fb8147d046
Marco Cristofano <mcristofano@linti.unlp.edu.ar>
2020-12-04 08:23:38 -0300

    clean code

--- simple_biblioteca/libros/api.py
+++ simple_biblioteca/libros/api.py
@@ -1,20 +1,14 @@
-from django.shortcuts import get_object_or_404
-
-from rest_framework.views import APIView
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework import status
 from rest_framework import generics
 
 from libros.serializers import (
     AutorSerializer,
-    LibroConAutorFullSerializer,
-    LibroConAutorlOutSerializer,
     LibroConAutorSerializer,
     LibroSerializer
-
 )
+
 from libros.models import (
     Autor,
     Libro
